6535d51a52f6155dcc07c22884e808f3544d3479
unknown <nbaalpha6@gmail.com>
2025-11-24 11:05:12 +0300

    hello

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('showTipBtn').addEventListener('click', () => {
+    const tipText = "Tip: Don't forget to flip the pancake when bubbles form on top!";
+    document.getElementById('tip').textContent = tipText;
+});
